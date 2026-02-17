c03b90bd3c770d604968ed5b452d625159f636c2
Иван Шихалев <shikhalev@gmail.com>
2026-02-17 06:28:14 +0500

    Вт 17 фев 2026 06:28:14 +05

--- lib/inat-get/data/parsers/photo.rb
+++ lib/inat-get/data/parsers/photo.rb
@@ -2,6 +2,11 @@
 
 class INatGet::Data::Parser::Photo < INatGet::Data::Parser
 
+  include Singleton
+
+  part Part::PK
+  part Part::Copy, :url, :license => :license_code
+
   # @return [class Model::Photo]
   def model() = INatGet::Data::Model::Photo
 
--- lib/inat-get/data/parsers/sound.rb
+++ lib/inat-get/data/parsers/sound.rb
@@ -2,6 +2,11 @@
 
 class INatGet::Data::Parser::Sound < INatGet::Data::Parser
 
+  include Singleton
+
+  part Part::PK
+  part Part::Copy, :url => :file_url, :license => :license_code
+
   # @return [class Model::Sound]
   def model() = INatGet::Data::Model::Sound
 
