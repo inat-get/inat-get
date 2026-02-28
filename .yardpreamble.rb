81a2d5048f85db465b1dab6adfa876c147e28ccd
Иван Шихалев <shikhalev@gmail.com>
2026-02-28 16:08:56 +0500

    minor

--- .yardpreamble.rb
+++ .yardpreamble.rb
@@ -1,2 +1,15 @@
-
 ENV['SEQUEL_MIGRATIONS_DIR'] = './share/inat-get/db/migrations/'
+
+module WF
+
+  def warn message, category = nil, **kwargs
+    case message
+    when /literal string will be frozen in the future/, /character class has duplicated range/, /yardpreamble/
+      # do nothing
+    else
+      super message
+    end
+  end
+
+end
+Warning.extend WF
