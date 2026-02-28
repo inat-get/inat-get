0863947b1011f16785276e9e36e2d69b353bc9c9
Иван Шихалев <shikhalev@gmail.com>
2026-02-28 10:15:55 +0500

    fix test

--- spec/unit/dsl_spec.rb
+++ spec/unit/dsl_spec.rb
@@ -31,16 +31,16 @@ RSpec::describe INatGet::Data::DSL do
   it 'check ranges' do
     obj = Wrapper::new
     today = Date::today
-    expect(obj.eval { range date: today }).to eq(today.to_datetime ... (today + 1).to_datetime)
+    expect(obj.eval { time_range date: today }).to eq(today.to_datetime ... (today + 1).to_datetime)
     start = Date::new(1901, 1, 1).to_datetime
     finish = Date::new(2001, 1, 1).to_datetime
-    expect(obj.eval { range century: 20 }).to eq(start ... finish)
+    expect(obj.eval { time_range century: 20 }).to eq(start ... finish)
     start = Date::new(2026, 4, 1).to_datetime
     finish = Date::new(2026, 7, 1).to_datetime
-    expect(obj.eval { range year: 2026, quarter: 2 }).to eq(start ... finish)
+    expect(obj.eval { time_range year: 2026, quarter: 2 }).to eq(start ... finish)
     start = Date::new(2025, 12, 1).to_datetime
     finish = Date::new(2026, 3, 1).to_datetime
-    expect(obj.eval { range year: 2026, season: :winter }).to eq(start ... finish)
+    expect(obj.eval { time_range year: 2026, season: :winter }).to eq(start ... finish)
   end
 
 end
