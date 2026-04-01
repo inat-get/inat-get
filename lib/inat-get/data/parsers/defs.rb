1ba7a03b19a6083e53afdabd865594dcce7a5c08
Иван Шихалев <shikhalev@gmail.com>
2026-04-01 07:19:45 +0500

    fix #130 (backport)

--- lib/inat-get/data/parsers/defs.rb
+++ lib/inat-get/data/parsers/defs.rb
@@ -105,7 +105,8 @@ class INatGet::Data::Parser
     end
     associations, attributes = rest.partition { |p| p.is_a?(Part::Assoc) }
     attributes.each do |a|
-      fields.merge! a.parse(source)
+      res = a.parse source
+      fields.merge! res if res
     end
     record = nil
     model.db.transaction(isolation: :committed, mode: :immediate) do
